8b24e0d3e364eb900af6925cfcc80cc7b524abee
Tony Blum <tonyblum@me.com>
2015-11-18 14:17:25 -0800

    User Authentication implemented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
     before_action :find_book, only: [:show, :edit, :update, :destroy]
 
+    before_action :authenticate_user!, only: [:new, :edit]
 
     def index
 
