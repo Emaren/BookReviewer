2cde73cf8c2fe16560265b5bee8ba374401db441
Tony Blum <tonyblum@me.com>
2015-11-18 09:41:33 -0800

    Paperclip implemented

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -60,7 +60,7 @@ class BooksController < ApplicationController
     end
 
     def book_params
-      params.require(:book).permit(:title, :description, :author, :category_id)
+      params.require(:book).permit(:title, :description, :author, :category_id, :book_img)
     end
 
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,8 @@ class Book < ActiveRecord::Base
     belongs_to :category
 
 
+    has_attached_file :book_img, styles: { book_index: "250x350>", :book_show => "325x475>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\Z/
+
+
 end
